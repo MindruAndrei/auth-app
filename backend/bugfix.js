80da38f74a765ba3c3255a9edda7e1fe0853fb85
MindruAndrei <amindru99@yahoo.com>
2023-11-27 11:15:24 +0200

    fix: bugfix

---
+++ backend/bugfix.js
@@ -0,0 +1,35 @@
+//fixed error handling so it exists the program if there is an error in the readFile function or the write function
+const fs = require("fs");
+function readFile(path, callback) {
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+}
+function writeFile(path, data, callback) {
+  fs.writeFile(path, data, "utf8", (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
+}
+
+readFile("/Users/mindruandrei/auth-app/backend/input.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const newData = data.toUpperCase();
+  writeFile("output.txt", newData, (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log("File written successfully");
+  });
+});
